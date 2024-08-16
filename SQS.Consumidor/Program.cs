493ce36d946e484d7bfaaeb2d68f53cc77766127
Raphael Soares <raphael.moreira@meuinc.com.br>
2024-08-16 14:28:09 -0300

    Mudança de url para variaveis de ambiente

--- SQS.Consumidor/Program.cs
+++ SQS.Consumidor/Program.cs
@@ -3,26 +3,15 @@ using Amazon;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 
-namespace SQS.Consumidor
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new ReceiveMessageRequest
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("Opa");
+    QueueUrl = "AWS_ENDPOINT_URL"
+};
+var response = await client.ReceiveMessageAsync(request);
 
-            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
-            var request = new ReceiveMessageRequest
-            {
-                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/583539974430/Teste_SQS"
-            };
-            var response = await client.ReceiveMessageAsync(request);
-
-            foreach (var message in response.Messages) 
-            { 
-                Console.WriteLine($"{message}");
-            }
-
-        }
-    }
+foreach (var message in response.Messages)
+{
+    Console.WriteLine(message.Body);
+    await client.DeleteMessageAsync("AWS_ENDPOINT_URL", message.ReceiptHandle);
 }
