404dee0b741b7eacfa0fee61988766e1e64c2ab5
Raphael Soares <raphael.moreira@meuinc.com.br>
2024-08-16 13:25:07 -0300

    Adicionar arquivos de projeto.

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumidor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Opa");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/583539974430/Teste_SQS"
+            };
+            var response = await client.ReceiveMessageAsync(request);
+
+            foreach (var message in response.Messages) 
+            { 
+                Console.WriteLine($"{message}");
+            }
+
+        }
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Fornecedor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Opa");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/583539974430/Teste_SQS",
+                MessageBody = "Teste envio"
+            };
+
+            await client.SendMessageAsync(request);
+        }
+    }
+}
